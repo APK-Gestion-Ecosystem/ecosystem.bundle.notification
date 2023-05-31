c8b3be7489335ad48f174f8962af660872a4c0d2
Francisco Pereira <ciscopere@gmail.com>
2023-05-30 23:28:04 -0300

    changes

--- src/DependencyInjection/EcosystemNotificationExtension.php
+++ src/DependencyInjection/EcosystemNotificationExtension.php
@@ -12,7 +12,7 @@ class EcosystemNotificationExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
 
         $configuration = new Configuration();
