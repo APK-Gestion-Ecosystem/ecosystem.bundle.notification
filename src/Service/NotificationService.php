e5b2cb40f46dee168b0ff364f4292ee6835a04da
Francisco Pereira <ciscopere@gmail.com>
2023-05-31 00:59:07 -0300

    Update NotificationService.php

--- src/Service/NotificationService.php
+++ src/Service/NotificationService.php
@@ -11,7 +11,8 @@ class NotificationService
     private HttpClientInterface $httpClient;
     private string $notificationUrl;
 
-    public function __construct(string $notificationUrl) {
+    public function __construct(string $notificationUrl)
+    {
         $this->notificationUrl = $notificationUrl;
         $this->httpClient = HttpClient::create([
             'headers' => [
@@ -20,7 +21,8 @@ class NotificationService
         ]);
     }
 
-    public function notify(Notification $notification): bool {
+    public function notify(Notification $notification): bool
+    {
         $response = $this->httpClient->request('POST', $this->notificationUrl, ['body' => json_encode($notification)]);
 
         return $response->getStatusCode() === 200;
