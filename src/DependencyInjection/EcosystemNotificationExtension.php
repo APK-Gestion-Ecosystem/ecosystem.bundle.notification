c4a03fe774b994ad7e3c5b028708c800a16676b3
Jose Morlet <joseantonio.morlet@sngular.com>
2024-06-24 10:59:07 -0300

    add type return EcosystemNotificationExtension

--- src/DependencyInjection/EcosystemNotificationExtension.php
+++ src/DependencyInjection/EcosystemNotificationExtension.php
@@ -10,7 +10,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class EcosystemNotificationExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
         $loader->load('services.yaml');
